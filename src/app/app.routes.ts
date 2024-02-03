68cb03ab1de6ca66252e8f63c89c60e1b8cc8f67
Hrushikesh <hrushikesh.surkar@greatplaceitservices.com>
2024-02-03 12:34:06 +0530

    git live 404 part2 commit 2

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -6,14 +6,4 @@ export const routes: Routes = [
     path: 'home',
     component: HomeComponent,
   },
-  {
-    path: '',
-    redirectTo: 'home',
-    pathMatch: 'full',
-  },
-  {
-    path: '**',
-    redirectTo: 'home',
-    pathMatch: 'full',
-  },
 ];
